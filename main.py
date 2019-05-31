1d20946130715f83132bcade6758365f681a008a
Todd Birchard <toddbirchard@gmail.com>
2019-05-31 17:24:08 -0400

    CORS issue

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ from db import Database
 
 def main(request):
     """Entry point."""
+    if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600',
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+
+        }
+        return ('', 204, headers)
+    headers = {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+        }
     db = Database()
     authorName = request.args.get('author')
     records = db.create_response(authorName)
